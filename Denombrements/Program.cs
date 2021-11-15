aa1b364ea60eefd1dbc5ffe6b5b3629050ab94ed
MCkeydev <mattcourrejou@gmail.com>
2021-11-15 14:27:57 +0100

    Code review et Correctifs du 15/11/2021

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -1,70 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Denombrements
 {
     class Program
     {
+        static string initChoix()
+        {
+            return "";
+        }
+        //Fonction qui retourne un bool, vérifie que le choix est valide;
+        static bool InvalidChoice(string arg)
+        {
+            if(arg != "0" && arg != "1" && arg != "2" && arg != "3") return true;
+            else  return false; 
+            
+        }
+        //Fonction utilisée pour le menu, retourne un string choix;
+        static string menu(string choix = "")
+        {
+            while(InvalidChoice(choix)) {
+                
+                    Console.WriteLine("Permutation ...................... 1");
+                    Console.WriteLine("Arrangement ...................... 2");
+                    Console.WriteLine("Combinaison ...................... 3");
+                    Console.WriteLine("Quitter .......................... 0");
+                    Console.Write("Choix :                            ");
+                    choix = Console.ReadLine();
+                if (InvalidChoice(choix))
+                {
+                    Console.WriteLine("saissez un choix valide !");
+                }
+            }
+            return choix;
+            
+
+        }
+      
         static void Main(string[] args)
         {
-            int c = 1;
-            while (c != 0)
+            //Initialisation du choix
+            string choix = initChoix();
+            while (choix != "0")
             {
-                Console.WriteLine("Permutation ...................... 1");
-                Console.WriteLine("Arrangement ...................... 2");
-                Console.WriteLine("Combinaison ...................... 3");
-                Console.WriteLine("Quitter .......................... 0");
-                Console.Write("Choix :                            ");
-                c = int.Parse(Console.ReadLine());
+                choix = menu(choix);
 
-                if (c == 0) { Environment.Exit(0); }
+                if (choix == "0") Environment.Exit(0);
 
-                if (c == 1)
-                {
-                    Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                    int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                                                           // calcul de r
+                Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                int nombre = int.Parse(Console.ReadLine()); // saisir le nombre
+
+                if (choix == "1")
+                {                                                                             
                     long r = 1;
-                    for (int k = 1; k <= n; k++)
+                    for (int k = 1; k <= nombre; k++)
+                    {
                         r *= k;
-                    Console.WriteLine(n + "! = " + r);
+                    }
+                    Console.WriteLine(nombre + "! = " + r);
+                    choix = initChoix();
                 }
                 else
                 {
-                    if (c == 2)
+                   
+                    Console.Write("nombre d'éléments dans le sous ensemble = "); //saisir le sous ensemble
+                    int sousEnsemble = int.Parse(Console.ReadLine()); 
+                   
+                    if (choix == "2")
                     {
-                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
-                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
-                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                        // calcul de r
-                        long r = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
-                            r *= k;
-                        //Console.WriteLine("résultat = " + (r1 / r2));
-                        Console.WriteLine("A(" + t + "/" + n + ") = " + r);
+                       // calcul de r
+                        long resultat = 1;
+                        for (int i = (nombre - sousEnsemble + 1); i <= nombre; i++)
+                            resultat *= i;
+                       //Calcul du resultat final
+                        Console.WriteLine("A(" + nombre + "/" + sousEnsemble + ") = " + resultat);
+                        choix = initChoix();
                     }
-                    else
+                    if (choix == "3")
                     {
-                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
-                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
-                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                        // calcul de r1
-                        long r1 = 1;
-                        for (int k = (t - n + 1); k <= t; k++)
-                            r1 *= k;
-                        // calcul de r2
-                        long r2 = 1;
-                        for (int k = 1; k <= n; k++)
-                            r2 *= k;
-                        // calcul de r3
-                        //Console.WriteLine("résultat = " + (r1 / r2));
-                        Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
+                        // calcul de resulat1
+                        long resultat1 = 1;
+                        for (int i = (nombre - sousEnsemble + 1); i <= nombre; i++)
+                            resultat1 *= i;
+
+                        // calcul de resultat2
+                        long resultat2 = 1;
+                        for (int i = 1; i <= sousEnsemble; i++)
+                            resultat2 *= i;
+                        
+                        //Calcul du resultat final
+                        Console.WriteLine("C(" + nombre + "/" + sousEnsemble + ") = " + (resultat1 / resultat2));
+                        choix = initChoix();
                     }
+                    
                 }
             }
             Console.ReadLine();
